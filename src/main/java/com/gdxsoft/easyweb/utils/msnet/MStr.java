8220662babd82f9c4ecdf328e76a5a1dd58fd3bb
admin <admin@host.docker.internal>
2022-08-03 07:35:00 +0800

    MStr return this

--- src/main/java/com/gdxsoft/easyweb/utils/msnet/MStr.java
+++ src/main/java/com/gdxsoft/easyweb/utils/msnet/MStr.java
@@ -10,7 +10,7 @@ import com.gdxsoft.easyweb.utils.Utils;
  * @author Administrator
  * 
  */
-public class MStr implements Serializable{
+public class MStr implements Serializable {
 
 	/**
 	 * 
@@ -18,17 +18,19 @@ public class MStr implements Serializable{
 	private static final long serialVersionUID = 1877502096998706509L;
 	private StringBuilder _Sb;
 
+	private String newLine = "\r\n";
+
 	public MStr() {
 		_Sb = new StringBuilder();
-	}
-
-	public int length() {
-		return _Sb.length();
 	}
 
 	public MStr(Object val) {
 		_Sb = new StringBuilder();
 		_Sb.append(val);
+	}
+
+	public int length() {
+		return _Sb.length();
 	}
 
 	public void setLength(int length) {
@@ -40,59 +42,153 @@ public class MStr implements Serializable{
 	}
 
 	/**
-	 * 添加内容
-	 * @param val
+	 * 添加内容 = append
+	 * 
+	 * @param val 内容
+	 * @return this
 	 */
-	public void a(Object val) {
+	public MStr a(Object val) {
 		_Sb.append(val);
-	}
-	
-	public void append(Object val) {
-		_Sb.append(val);
-	}
-
-	public void appendLine(Object val) {
-		_Sb.append(val);
-		_Sb.append("\r\n");
+		return this;
 	}
 
 	/**
-	 * 增加新行
-	 * @param val
+	 * 添加内容
+	 * 
+	 * @param val 内容
+	 * @return this
 	 */
-	public void al(Object val) {
-		this.appendLine(val);
+	public MStr append(Object val) {
+		_Sb.append(val);
+		return this;
 	}
 
+	/**
+	 * 添加内容，并在结尾添加新行(newLine)，默认\r\n
+	 * 
+	 * @param val 内容
+	 * @return this
+	 */
+	public MStr appendLine(Object val) {
+		_Sb.append(val);
+		_Sb.append(this.newLine);
+		return this;
+	}
+
+	/**
+	 * 添加内容，并在结尾添加新行(newLine)，默认\r\n = appendLine
+	 * 
+	 * @param val 内容
+	 * @return this
+	 */
+	public MStr al(Object val) {
+		this.appendLine(val);
+		return this;
+	}
+
+	/**
+	 * 查找字符串位置
+	 * 
+	 * @param val 字符串
+	 * @return
+	 */
 	public int indexOf(String val) {
 		return _Sb.indexOf(val);
 	}
 
+	/**
+	 * 查找字符串位置
+	 * 
+	 * @param val   字符串
+	 * @param start 开始位置
+	 * @return
+	 */
 	public int indexOf(String val, int start) {
 		return _Sb.indexOf(val, start);
 	}
 
-	public String substring(int startIndex) {
-		return _Sb.substring(startIndex);
+	/**
+	 * 提取字符串
+	 * 
+	 * @param startIndex 开始位置
+	 * @return 提取的字符串
+	 */
+	public MStr substring(int startIndex) {
+		_Sb.substring(startIndex);
+		return this;
 	}
 
+	/**
+	 * 提取字符串
+	 * 
+	 * @param startIndex 开始位置
+	 * @param endIndex   结束位置
+	 * @return 提取的字符串
+	 */
 	public String substring(int startIndex, int endIndex) {
 		return _Sb.substring(startIndex, endIndex);
 	}
 
-	public void delete(int startIndex, int endIndex) {
+	/**
+	 * 删除字符串
+	 * 
+	 * @param startIndex 开始位置
+	 * @param endIndex   结束位置
+	 * @return this
+	 */
+	public MStr delete(int startIndex, int endIndex) {
 		_Sb.delete(startIndex, endIndex);
+		return this;
 	}
 
-	public void insert(int index, Object val) {
+	/**
+	 * 插入字符串
+	 * 
+	 * @param index 位置
+	 * @param val   字符串
+	 * @return this
+	 */
+	public MStr insert(int index, Object val) {
 		_Sb.insert(index, val);
+		return this;
 	}
 
-	public void replace(String find, String val) {
+	/**
+	 * 只替换一次
+	 * 
+	 * @param find
+	 * @param val
+	 * @return this
+	 */
+	public MStr replace(String find, String val) {
 		Utils.replaceStringBuilder(_Sb, find, val);
+		return this;
 	}
 
+	/**
+	 * 返回字符串
+	 * 
+	 * @return string
+	 */
 	public String toString() {
 		return _Sb.toString();
+	}
+
+	/**
+	 * 新行字符串，默认是\r\n
+	 * 
+	 * @return the newLine
+	 */
+	public String getNewLine() {
+		return newLine;
+	}
+
+	/**
+	 * 设置新行字符串，默认是\r\n
+	 * 
+	 * @param newLine the newLine to set
+	 */
+	public void setNewLine(String newLine) {
+		this.newLine = newLine;
 	}
 }
